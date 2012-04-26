a9c691701f363f09afa93cdf24bf67100a213b93
Heiko Dreyer <mail@boxedfolder.com>
2012-04-26 22:23:29 +0200

    Init Commit

---
+++ BFNavigationControllerExample/BFNavigationControllerExample/BFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BFAppDelegate.h
+//  BFNavigationControllerExample
+//
+//  Created by Heiko Dreyer on 26.04.12.
+//  Copyright (c) 2012 boxedfolder.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BFAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ BFNavigationControllerExample/BFNavigationControllerExample/BFAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  BFAppDelegate.m
+//  BFNavigationControllerExample
+//
+//  Created by Heiko Dreyer on 26.04.12.
+//  Copyright (c) 2012 boxedfolder.com. All rights reserved.
+//
+
+#import "BFAppDelegate.h"
+
+@implementation BFAppDelegate
+
+@synthesize window = _window;
+
+-(void)applicationDidFinishLaunching: (NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
